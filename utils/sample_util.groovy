448f85c45d8c41f43f10dc3363c2e0b0a442851d
BardicheAssualt <akito_akegarasu@hotmail.co.jp>
2019-08-10 18:08:09 +0900

    コンソールテキストの成果物保存追加

--- utils/sample_util.groovy
+++ utils/sample_util.groovy
@@ -9,4 +9,15 @@ def getConsoleText() {
 	return ConsoleNote.removeNotes(currentBuild.rawBuild.log)
 }
 
+/**
+ * この関数を実行した時点までのコンソールテキストを成果物として保存します
+ */
+def archiveConsoleText() {
+	dir('archiveConsoleText') {
+		writeFile encoding: 'utf-8', file: 'consoleText.txt', text: consoleText
+		archiveArtifacts 'consoleText.txt'
+		deleteDir()
+	}
+}
+
 return this
